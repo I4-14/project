761f527c0387f57c3275b76ea787731b6f30a363
kyungtae42 <kimbam1017@naver.com>
2024-07-10 17:22:25 +0900

    Column 레포지토리 생성

---
+++ src/main/java/com/sparta/trello/columns/repository/ColumnsRepository.java
@@ -0,0 +1,8 @@
+package com.sparta.trello.columns.repository;
+
+import com.sparta.trello.columns.entity.Columns;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ColumnsRepository extends JpaRepository<Columns, Long> {
+
+}
