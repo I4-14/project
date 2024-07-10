987cf996ea356486735291195955a942cfc50616
hgi4657 <120237927+hgi4657@users.noreply.github.com>
2024-07-10 17:28:54 +0900

    Revert "Feat/#1 column"

--- src/main/java/com/sparta/trello/TrelloApplication.java
+++ src/main/java/com/sparta/trello/TrelloApplication.java
@@ -2,10 +2,8 @@ package com.sparta.trello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class TrelloApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/sparta/trello/columns/repository/ColumnsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.trello.columns.repository;
-
-import com.sparta.trello.columns.entity.Columns;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ColumnsRepository extends JpaRepository<Columns, Long> {
-
-}
