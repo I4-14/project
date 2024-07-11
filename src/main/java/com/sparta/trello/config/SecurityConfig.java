364f5fb0bcc7045b9a19a1a70a3351b69f24f654
hgi4657 <hgi4657@naver.com>
2024-07-11 16:41:21 +0900

    ✨ update : Security 설정

--- src/main/java/com/sparta/trello/config/SecurityConfig.java
+++ src/main/java/com/sparta/trello/config/SecurityConfig.java
@@ -5,15 +5,18 @@ import com.sparta.trello.jwt.JwtAuthenticationFilter;
 import com.sparta.trello.jwt.JwtAuthorizationFilter;
 import com.sparta.trello.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -47,6 +50,22 @@ public class SecurityConfig {
         return new JwtAuthorizationFilter(jwtUtil, userDetailsService);
     }
 
+    // 보안 필터 설정
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정
+        http.csrf((csrf) -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((auth) -> auth
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+                                .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
+                                .requestMatchers("/api/auth/signup", "/api/auth/login").permitAll() // 접근 허가
+                                .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+                );
 
+        http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
+        return http.build();
+    }
 }
