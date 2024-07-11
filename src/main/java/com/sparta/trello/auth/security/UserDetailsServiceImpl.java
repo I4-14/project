9ffa967cf0c6f46a9cf4bdc7c96947e039f6b696
hgi4657 <hgi4657@naver.com>
2024-07-12 02:15:20 +0900

    🚚 move : 탈퇴유저 검증 코드 이동 - 실행되지 않는 것으로 확인

--- src/main/java/com/sparta/trello/auth/security/UserDetailsServiceImpl.java
+++ src/main/java/com/sparta/trello/auth/security/UserDetailsServiceImpl.java
@@ -3,6 +3,7 @@ package com.sparta.trello.auth.security;
 import com.sparta.trello.auth.entity.User;
 import com.sparta.trello.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,10 +19,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 사용자입니다."));
-
-        if (!user.isExist()) {
-            throw new UsernameNotFoundException("탈퇴한 사용자입니다.");
-        }
 
         return new UserDetailsImpl(user);
     }
