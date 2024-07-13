5853b74dac7e78afed7e5266fc8db901d75130da
hgi4657 <hgi4657@naver.com>
2024-07-13 11:58:38 +0900

    ✨ update : Auth RequestDto Validation 설정 추가 및 수정

--- src/main/java/com/sparta/trello/auth/dto/LoginRequestDto.java
+++ src/main/java/com/sparta/trello/auth/dto/LoginRequestDto.java
@@ -1,9 +1,17 @@
 package com.sparta.trello.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class LoginRequestDto {
+    @NotBlank(message = "사용자 ID는 필수 입력 사항입니다.")
+    @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "사용자 ID는 알파벳 소문자와 숫자로 이루어진 4자에서 10자 사이여야 합니다.")
     private String username;
+
+    @NotBlank(message = "비밀번호는 필수 입력 사항입니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$",
+            message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자(az, AZ), 숫자(0~9),특수문자로 구성되어야 합니다.")
     private String password;
 }
