26512ec5e13892c11b5b8ca4b55b7ee26b8969eb
yoodab <yoondab2166@gmail.com>
2024-07-15 13:30:00 +0900

    🩹 fix : builder로 수정

--- src/main/java/com/sparta/trello/board/controller/BoardController.java
+++ src/main/java/com/sparta/trello/board/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.sparta.trello.board.controller;
 
 import com.sparta.trello.auth.entity.User;
+import com.sparta.trello.auth.security.UserDetailsImpl;
 import com.sparta.trello.board.dto.BoardRequestDto;
 import com.sparta.trello.board.dto.BoardResponseDto;
 import com.sparta.trello.board.service.BoardService;
@@ -8,6 +9,7 @@ import com.sparta.trello.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,65 +21,53 @@ public class BoardController {
 
     private final BoardService boardService;
 
-    // 임시 유저
-    User temUser = new User();
 
-    /**
-     * 보드 목록 조회
-     * get
-     * /api/boards
-     */
     @GetMapping("/boards")
     public ResponseEntity<ApiResponse<List<BoardResponseDto>>> getBoardlist(@RequestParam(value = "page", defaultValue = "1") int page,
-                                                                            @RequestParam(value = "sortBy", defaultValue = "createdAt") String sortBy) {
-        List<BoardResponseDto> boardList = boardService.getBoardList(page - 1, sortBy);
-        ApiResponse<List<BoardResponseDto>> response = new ApiResponse<>("보드 목록 조회 성공", "200", boardList);
+                                                                            @RequestParam(value = "sortBy", defaultValue = "createdAt") String sortBy,
+                                                                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        List<BoardResponseDto> boardList = boardService.getBoardList(page - 1, sortBy,userDetails.getUser());
+
+        ApiResponse response = ApiResponse.builder()
+                .msg("보드 목록 조회 성공")
+                .statuscode(String.valueOf(HttpStatus.OK.value()))
+                .data(boardList)
+                .build();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    /**
-     * 보드 생성
-     * post
-     * /api/boards
-     * {
-     * ”title” : “보드제목”,
-     * "description": "보드설명"
-     * }
-     */
+
     @PostMapping("/boards")
     public ResponseEntity<ApiResponse<BoardResponseDto>> createBoard(@RequestBody BoardRequestDto boardRequestDto) {
         BoardResponseDto boardResponseDto = boardService.createBoard(boardRequestDto);
-        ApiResponse<BoardResponseDto> response = new ApiResponse<>("보드 생성 성공", "201", boardResponseDto);
-
+        ApiResponse response = ApiResponse.builder()
+                .msg("보드 생성 성공")
+                .statuscode(String.valueOf(HttpStatus.CREATED.value()))
+                .data(boardResponseDto)
+                .build();
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    /**
-     * 보드 수정
-     * put
-     * /api/boards/{boardId}
-     * {
-     * ”title” : “보드제목”,
-     * "description": "보드설명"
-     * }
-     */
+
     @PutMapping("/boards/{boardId}")
     public ResponseEntity<ApiResponse<BoardResponseDto>> updateBoard(@PathVariable("boardId") Long boardId, @RequestBody BoardRequestDto boardRequestDto) {
         BoardResponseDto boardResponseDto = boardService.updateBoard(boardId, boardRequestDto);
-        ApiResponse<BoardResponseDto> response = new ApiResponse<>("보드 수정 성공", "200", boardResponseDto);
+        ApiResponse response = ApiResponse.builder()
+                .msg("보드 수정 성공")
+                .statuscode(String.valueOf(HttpStatus.OK.value()))
+                .data(boardResponseDto)
+                .build();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 
-    /**
-     * 보드 삭제
-     * delete
-     * /api/boards/{boardId}
-     */
     @DeleteMapping("/boards/{boardId}")
     public ResponseEntity<ApiResponse> deleteBoard(@PathVariable("boardId") Long boardId) {
-        boardService.deleteBoard(boardId, temUser);
-        ApiResponse<Void> response = new ApiResponse("보드 삭제 성공", "204", null);
+        boardService.deleteBoard(boardId);
+        ApiResponse response = ApiResponse.builder()
+                .msg("보드 삭제 성공")
+                .statuscode(String.valueOf(HttpStatus.NO_CONTENT.value()))
+                .build();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
